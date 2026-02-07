f218b2de4fdb77b9da84a6c90e781f9e076cf3bf
Jakub <work.jhanko@gmail.com>
2026-02-07 23:49:39 +0100

    Final working code and correct data I wanted. Add exceptions for writing files and all visit func. Add series and material to racket struct.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"regexp"
 	"strings"
@@ -17,37 +18,56 @@ type Racket struct {
 	RacketPage string `json:"racketPage"`
 	Weight     string `json:"weight"`
 	Shape      string `json:"shape"`
+	Material   string `json:"material"`
+	Series     string `json:"series"`
 }
 
-func ScrapRacketPage(url string) (Weight string, Shape string) {
-	regexWeight := regexp.MustCompile(`(?i)\bweight\s*:\s*([0-9]{2,4}\s*(?:[-–]\s*[0-9]{2,4})?\s*g)\b`)
-	regexShape := regexp.MustCompile(`(?i)\bshape\s*:\s*([^\r\n•]+)`)
+var regexWeight = regexp.MustCompile(`(?im)\b(?:approx(?:imate)?\.?\s*)?weight\s*(?:[:·-])\s*([0-9]{2,4}(?:\s*[-–]\s*[0-9]{2,4})?\s*(?:g|grs?|grams?))\.?`)
+var regexShape = regexp.MustCompile(`(?im)\b(?:shape|form)\s*(?:[:·-])\s*(hybrid|diamond|round|geometric|teardrop|tear\s*drop)\b`)
+var regexMaterial = regexp.MustCompile(`(?im)[•\s]*\b(?:comp\.?\s*exterior(?:\s*composition)?|exterior\s*comp\.?(?:osition)?|exterior\s*composition|outer\s*comp\.?(?:osition)?|outer\s*composition|outer\s*shell)\s*(?:[:·-])\s*([^•\r\n]+?)(?:\.\s*-\s*|•|\r?\n|$)`)
 
-	var weight string = ""
-	var shape string = ""
+func ScrapeRacketPage(url string) (Weight string, Shape string, Material string) {
+
+	weight := ""
+	shape := ""
+	material := ""
 
 	c := colly.NewCollector(colly.AllowedDomains("www.bullpadel.com"))
 	c.OnHTML("div[class='description-short']", func(e *colly.HTMLElement) {
-		Description := e.ChildText("p")
+		Description := e.Text
+
 		tmpWeight := regexWeight.FindStringSubmatch(Description)
 		if len(tmpWeight) >= 2 {
 			weight = strings.ReplaceAll(strings.TrimSpace(tmpWeight[1]), " ", "")
 		}
+
 		tmpShape := regexShape.FindStringSubmatch(Description)
 		if len(tmpShape) >= 2 {
 			shape = tmpShape[1]
 		}
+
+		tmpMaterial := regexMaterial.FindStringSubmatch(Description)
+		if len(tmpMaterial) >= 2 {
+			material = tmpMaterial[1]
+		}
 	})
 
-	c.Visit(url)
-	return weight, shape
+	if err := c.Visit(url); err != nil {
+		fmt.Println("visit error: ", err)
+	}
+	return weight, shape, material
 }
 
-func ScrapRacket(url string) {
+func ScrapeRacket(url string) {
 	var items []Racket
 	c := colly.NewCollector(colly.AllowedDomains("www.bullpadel.com"))
 
 	series := strings.Split(url, "/")[4]
+	series = strings.ReplaceAll(series, "-", " ")
+	series = strings.Title(series)
+	partCut := strings.Split(series, " ")[0]
+	series = strings.ReplaceAll(series, partCut, "")
+	series = strings.ReplaceAll(series, " ", "")
 
 	c.OnHTML("div.left-column div[class='thumbnail-container']", func(e *colly.HTMLElement) {
 		item := Racket{
@@ -57,31 +77,54 @@ func ScrapRacket(url string) {
 			ImageUrl:   e.ChildAttr("img", "src"),
 			RacketPage: e.ChildAttr("a", "href"),
 		}
+		if strings.Contains(item.Model, "PACK") {
+			return
+		}
 		item.Model = strings.Replace(item.Model, "BULLPADEL", "", 1)
 		item.Model = strings.Replace(item.Model, "RACKET", "", 1)
-		item.Model = strings.Replace(item.Model, "PACK", "", 1)
 		item.Model = strings.Replace(item.Model, " ", "", 1)
-		weight, shape := ScrapRacketPage(item.RacketPage)
+
+		if strings.HasPrefix(item.RacketPage, "/") {
+			item.RacketPage = "https://www.bullpadel.com" + item.RacketPage
+		}
+
+		weight, shape, material := ScrapeRacketPage(item.RacketPage)
+
+		item.Series = series
 		item.Weight = weight
 		item.Shape = shape
+
+		material = strings.TrimSpace(material)
+		material = strings.TrimRight(material, ". ")
+		item.Material = material
+
 		items = append(items, item)
 	})
 
 	c.OnHTML("nav[class='pagination']", func(e *colly.HTMLElement) {
 		nextPage := e.ChildAttr("a[rel='next']", "href")
-		c.Visit(nextPage)
+		if nextPage != "" {
+			if err := c.Visit(nextPage); err != nil {
+				fmt.Println("next page visit error: ", err)
+			}
+		}
 	})
 
-	c.Visit(url)
+	if err := c.Visit(url); err != nil {
+		fmt.Println("visit error: ", err)
+	}
 
 	content, err := json.Marshal(items)
 	if err != nil {
-		panic(err)
+		fmt.Println("error: ", err)
+		return
 	}
-	os.WriteFile("BullPadelRackets"+series+".json", content, 0644)
+	if err := os.WriteFile("BullPadelRackets"+series+".json", content, 0644); err != nil {
+		fmt.Println("error: ", err)
+	}
 }
 
 func main() {
-	ScrapRacket("https://www.bullpadel.com/gb/39-proline")
-	ScrapRacket("https://www.bullpadel.com/gb/234-ltd-collection")
+	ScrapeRacket("https://www.bullpadel.com/gb/39-proline")
+	ScrapeRacket("https://www.bullpadel.com/gb/234-ltd-collection")
 }
