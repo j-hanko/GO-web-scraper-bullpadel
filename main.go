fe28e9c4f83412c79b62437fd8b4cba8b92410c9
Jakub <work.jhanko@gmail.com>
2026-02-07 15:35:00 +0100

    Now it's importing data with weight and shape, not work perfectly for every racket

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/gocolly/colly"
@@ -19,7 +20,31 @@ type Racket struct {
 
 var items []Racket
 
-func Scrap(url string) {
+func ScrapRacketPage(url string) (Weight string, Shape string) {
+	regexWeight := regexp.MustCompile(`(?i)\bweight\s*:\s*([0-9]{2,4}\s*(?:[-–]\s*[0-9]{2,4})?\s*g)\b`)
+	regexShape := regexp.MustCompile(`(?i)\bshape\s*:\s*([^\r\n•]+)`)
+
+	var weight string = ""
+	var shape string = ""
+
+	c := colly.NewCollector(colly.AllowedDomains("www.bullpadel.com"))
+	c.OnHTML("div[class='description-short']", func(e *colly.HTMLElement) {
+		Description := e.ChildText("p")
+		tmpWeight := regexWeight.FindStringSubmatch(Description)
+		if len(tmpWeight) >= 2 {
+			weight = strings.ReplaceAll(strings.TrimSpace(tmpWeight[1]), " ", "")
+		}
+		tmpShape := regexShape.FindStringSubmatch(Description)
+		if len(tmpShape) >= 2 {
+			shape = tmpShape[1]
+		}
+	})
+
+	c.Visit(url)
+	return weight, shape
+}
+
+func ScrapRacket(url string) {
 	c := colly.NewCollector(colly.AllowedDomains("www.bullpadel.com"))
 
 	series := strings.Split(url, "/")[4]
@@ -34,6 +59,9 @@ func Scrap(url string) {
 		item.Model = strings.Replace(item.Model, "RACKET", "", 1)
 		item.Model = strings.Replace(item.Model, "PACK", "", 1)
 		item.Model = strings.Replace(item.Model, " ", "", 1)
+		weight, shape := ScrapRacketPage(item.RacketPage)
+		item.Weight = weight
+		item.Shape = shape
 		items = append(items, item)
 	})
 
@@ -52,6 +80,6 @@ func Scrap(url string) {
 }
 
 func main() {
-	Scrap("https://www.bullpadel.com/gb/39-proline")
-	Scrap("https://www.bullpadel.com/gb/234-ltd-collection")
+	ScrapRacket("https://www.bullpadel.com/gb/39-proline")
+	ScrapRacket("https://www.bullpadel.com/gb/234-ltd-collection")
 }
